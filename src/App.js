bac36eb39993d2ee723a0a10fb2deacf3c7c6de2
dallocdalloc <dwpark@donga.ac.kr>
2025-04-16 14:02:02 +0900

    #7.2 Coin Tracker Challenge

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const onChange = (event) => {
+    setMoney(event.target.value);
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
-        setCoins(json);
-        setLoading(false);
+        setCoins(json.filter((item) => item.symbol === "BTC"));
       })
-  }, []);
+    }, []);
   console.log(coins);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-            ))}
-        </select>
-      )}
+      <h1>Bitcoin!</h1>
+      <input onChange={onChange} value={money} type="text" placeholder="Write your money."></input>
+      {
+        <h4>i'll buy {coins[0].quotes.USD.price / money} qty</h4>
+      }
+      {
+        coins.map((coin) => (
+          <h4>
+          {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </h4>
+        ))
+      }
     </div>
   );
 }
